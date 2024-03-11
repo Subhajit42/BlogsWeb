aa841f0a4026249b76f56ff06e316d5d3fa5127e
unknown <subhajit1742@gmail.com>
2024-03-12 00:03:36 +0530

    Added Firebase config file

---
+++ src/config/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { browserLocalPersistence, getAuth, GoogleAuthProvider, setPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+await setPersistence(auth, browserLocalPersistence);
+export const persistance = auth.setPersistence(browserLocalPersistence);
+
+
+
+export const GoogleAuth = new GoogleAuthProvider(app);
+export const db = getFirestore(app);
